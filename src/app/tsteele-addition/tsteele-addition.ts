7425f6b9802f053c28fd45ee4ff76ceb7cfb740b
tsteelematc <tsteele@madisoncollege.edu>
2026-03-06 14:56:45 -0600

    Styled inputs and buttons, good enough...

--- src/app/tsteele-addition/tsteele-addition.ts
+++ src/app/tsteele-addition/tsteele-addition.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-tsteele-addition',
   imports: [
     MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   templateUrl: './tsteele-addition.html',
   styleUrl: './tsteele-addition.css',
