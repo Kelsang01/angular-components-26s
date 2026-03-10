606872b13d89376a772df1db77df858b088016dd
THamilton1123 <thamilton12@madisoncollege.edu>
2026-03-10 18:13:34 -0500

    ng generate component thamilton-rollD20 --skip-tests

---
+++ src/app/thamilton-roll-d20/thamilton-roll-d20.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thamilton-roll-d20',
+  imports: [],
+  templateUrl: './thamilton-roll-d20.html',
+  styleUrl: './thamilton-roll-d20.css',
+})
+export class ThamiltonRollD20 {
+
+}
