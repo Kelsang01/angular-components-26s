c6531f9f2e9b481cbd31d9d44013da48a2eff0f1
Michael Rooney <msroone@gmail.com>
2026-03-14 16:21:50 -0500

    needed some additional toppings

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -12,6 +12,14 @@ const dummyToppingsFromApi = [
     price: 3.50,
   },
   {
+    name: 'Mushrooms',
+    price: 2.75,
+  },
+  {
+    name: 'Anchovies',
+    price: 4.25,
+  },
+  {
     name: 'Saussage',
     price: 4.25,
   },
