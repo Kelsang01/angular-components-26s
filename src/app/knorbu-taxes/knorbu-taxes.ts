f4d4c5bd390630334de5eb37d7362c63048b2fd7
Kelsang Norbu <knorbu@edgewood.edu>
2026-04-06 09:25:03 -0500

    initialized number and total

--- src/app/knorbu-taxes/knorbu-taxes.ts
+++ src/app/knorbu-taxes/knorbu-taxes.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-knorbu-taxes',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormField,
+    MatInputModule
+  ],
   templateUrl: './knorbu-taxes.html',
   styleUrl: './knorbu-taxes.css',
 })
 export class KnorbuTaxes {
-
+    protected readonly inputNmber = signal(2)
+    protected readonly total = signal (4)
 }
