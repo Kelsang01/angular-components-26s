6d0f71336cd2ca851c19589d40d0e82e6a902eca
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-07 13:59:04 +0000

    Implemented Material Design in component

--- src/app/csnider-incrementer/csnider-incrementer.ts
+++ src/app/csnider-incrementer/csnider-incrementer.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-csnider-incrementer',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './csnider-incrementer.html',
   styleUrl: './csnider-incrementer.css',
 })
+
 export class CsniderIncrementer {
 
 }
